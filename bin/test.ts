cfc4d1e49e457347dbca3a31c8251466a759a9b6
杨志豪 <yangzhihao03@zuoyebang.com>
2021-12-11 16:13:19 +0800

    chore: test

--- bin/test.ts
+++ bin/test.ts
@@ -1,12 +1,5 @@
-/*
- * @Author: 杨志豪
- * @Date: 2021-12-11 16:01:05
- * @LastEditTime: 2021-12-11 16:11:20
- * @Description:
- */
 const a = 2
-
+const a = 3
 const b = 3
-const c = 1
 
 console.log(a)
